67fc38c924a27831fc41f5ed318b88712cafea4b
richellepintucan <richellepintucan@yahoo.com>
2024-03-28 15:46:12 +0100

    html css js files modified

--- src/init.js
+++ src/init.js
@@ -1,1 +1,34 @@
-alert('init.js loaded!');
+//data
+
+const data = {
+  count: 0,
+};
+
+// DOM
+const dom = {
+  display: document.getElementById('display'),
+  increment: document.getElementById('increment'),
+  decrement: document.getElementById('decrement'),
+  reset: document.getElementById('reset'),
+};
+
+// Handler
+const updateDisplay = () => {
+  dom.display.innerText = data.count;
+};
+
+// Events
+dom.increment.addEventListener('click', () => {
+  data.count += 1;
+  updateDisplay();
+});
+
+dom.decrement.addEventListener('click', () => {
+  data.count -= 1;
+  updateDisplay();
+});
+
+dom.reset.addEventListener('click', () => {
+  data.count = 0;
+  updateDisplay();
+});
